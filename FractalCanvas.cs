76371f6fcba3eb74faf2d9e7322f7d91d28fb521
Egor Gorbunov <egor.dmit@bk.ru>
2023-10-20 17:38:04 +0500

    Добавьте файлы проекта.

---
+++ FractalCanvas.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+using System.Numerics;
+using System;
+using AlgLab2;
+using System.Collections.Generic;
+
+namespace AlgLab2
+{
+    public class FractalCanvas : Canvas
+    {
+        public void DrawSierpinskiTriangle(Point p1, Point p2, Point p3, int depth)
+        {
+            if (depth == 0)
+            {
+                // Отрисовываем треугольник
+                var polygon = new Polygon();
+                polygon.Points.Add(p1);
+                polygon.Points.Add(p2);
+                polygon.Points.Add(p3);
+                polygon.Stroke = Brushes.Black;
+                polygon.StrokeThickness = 1;
+                polygon.Fill = Brushes.Black; // Закрасить треугольник
+                this.Children.Add(polygon);
+            }
+            else
+            {
+                // Разбиваем текущий треугольник на 3 более мелких и отрисовываем фрактал для каждого
+                var mid1 = new Point((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);
+                var mid2 = new Point((p2.X + p3.X) / 2, (p2.Y + p3.Y) / 2);
+                var mid3 = new Point((p1.X + p3.X) / 2, (p1.Y + p3.Y) / 2);
+
+                DrawSierpinskiTriangle(p1, mid1, mid3, depth - 1);
+                DrawSierpinskiTriangle(mid1, p2, mid2, depth - 1);
+                DrawSierpinskiTriangle(mid3, mid2, p3, depth - 1);
+            }
+        }
+        public void DrawSierpinskiCarpet(int depth, double x, double y, double size)
+        {
+            if (depth == 0)
+            {
+                // Отрисовываем прямоугольник
+                var rectangle = new Rectangle
+                {
+                    Width = size,
+                    Height = size,
+                    Fill = Brushes.Black
+                };
+
+                this.Children.Add(rectangle);
+                Canvas.SetLeft(rectangle, x);
+                Canvas.SetTop(rectangle, y);
+            }
+            else
+            {
+                double newSize = size / 3;
+                double newSize2 = 2 * newSize;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (i != 1 || j != 1)
+                        {
+                            double newX = x + i * newSize;
+                            double newY = y + j * newSize;
+
+                            DrawSierpinskiCarpet(depth - 1, newX, newY, newSize);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+
+}
+
+
+
+
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlgLab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //FractalCanvas fractalCanvas = new FractalCanvas();
+            //fractalCanvas.DrawSierpinskiCarpet(3, 150, 10, 500);
+            //Content = fractalCanvas;
+
+            //FractalCanvas fractalCanvas = new FractalCanvas();
+            //fractalCanvas.DrawSierpinskiTriangle(new Point(400, 25), new Point(100, 425), new Point(700, 425), 10);
+            //Content = fractalCanvas;
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            // Обработчик выбора фрактала
+            RadioButton radioButton = (RadioButton)sender;
+            // Здесь можно выполнять действия в зависимости от выбранного фрактала
+        }
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            int depth;
+            if (int.TryParse(depthTextBox.Text, out depth))
+            {
+                // Получить значение глубины из текстового поля
+
+                if (triangleRadioButton.IsChecked == true)
+                {
+                    FractalCanvas fractalCanvas = new FractalCanvas();
+                    fractalCanvas.DrawSierpinskiTriangle(new Point(400, 25), new Point(100, 425), new Point(700, 425), depth);
+                    Content = fractalCanvas;
+                }
+                else if (carpetRadioButton.IsChecked == true)
+                {
+                    FractalCanvas fractalCanvas = new FractalCanvas();
+                    fractalCanvas.DrawSierpinskiCarpet(depth, 150, 10, 500);
+                    Content = fractalCanvas;
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное значение глубины.");
+            }
+        }
+    }
+}
